88114087200afcc8884225f22a25451c412fde19
devolart <125961492+devolart@users.noreply.github.com>
2023-11-29 19:42:35 +0700

    Update config.py

--- config.py
+++ config.py
@@ -39,8 +39,15 @@ UPSTREAM_BRANCH = os.environ.get("UPSTREAM_BRANCH", "master")
 DB_URI = os.environ.get("DATABASE_URL", "")
 
 # ID dari Channel Atau Group Untuk Wajib Subscribenya
-FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL", "0"))
-FORCE_SUB_GROUP = int(os.environ.get("FORCE_SUB_GROUP", "0"))
+FORCE_SUB = {}
+FORCE_SUB_COUNTER = 1
+while True:
+    key = f"FORCE_SUB{FORCE_SUB_COUNTER}"
+    value = os.environ.get(key)
+    if value is None:
+        break
+    FORCE_SUB[FORCE_SUB_COUNTER] = int(value)
+    FORCE_SUB_COUNTER += 1
 
 TG_BOT_WORKERS = int(os.environ.get("TG_BOT_WORKERS", "4"))
 
